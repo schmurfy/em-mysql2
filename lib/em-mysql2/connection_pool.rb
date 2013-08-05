30cd7813c11da21d061263d0317b6e98134148f4
Julien Ammous <schmurfy@gmail.com>
2013-08-05 15:18:36 +0200

    proper activerecord 4 support

--- lib/em-mysql2/connection_pool.rb
+++ lib/em-mysql2/connection_pool.rb
@@ -9,25 +9,25 @@ module ActiveRecord
     # fibers running in the same thread don't try to use the same connection.
     class ConnectionPool
             
-    #   def new_initialize(*args, &block)
-    #     @monitor = FiberTools::Monitor.new
-    #     initialize_original(*args, &block)
+      def new_initialize(*args, &block)
+        @monitor = FiberTools::Monitor.new
+        initialize_original(*args, &block)
         
-    #     @reserved_connections = {}
-    #     @available = ActiveRecord::ConnectionAdapters::ConnectionPool::Queue.new( FiberTools::Monitor.new )
-    #   end
+        @reserved_connections = {}
+        @available = ActiveRecord::ConnectionAdapters::ConnectionPool::Queue.new( @monitor )
+      end
       
-    #   alias_method :initialize_original, :initialize
-    #   alias_method :initialize, :new_initialize
+      alias_method :initialize_original, :initialize
+      alias_method :initialize, :new_initialize
 
       def current_connection_id
         Base.connection_id ||= Fiber.current.object_id
       end
       
             
-    #   def synchronize(&block)
-    #     @monitor.synchronize(&block)
-    #   end
+      def synchronize(&block)
+        @monitor.synchronize(&block)
+      end
       
     end
     
